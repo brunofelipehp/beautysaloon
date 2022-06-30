ee78811691049f2c963841a05506674b84298c64
brunofelipehp <brunofelipehp8@gmail.com>
2022-06-30 17:00:58 -0300

    stage 03 finished

--- main.js
+++ main.js
@@ -41,3 +41,21 @@ const swiper = new Swiper('.swiper', {
   mousewheel: true,
   keyboard: true,
 });
+
+
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(`
+#home .text, #home .image,
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimonials,
+#contact .text, #contact .links
+`,
+{ interval: 100 })
